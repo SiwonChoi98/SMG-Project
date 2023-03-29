2366a9f63aac94f07fb06d978e931e6dc6bf0745
SiwonChoi98 <118884862+SiwonChoi98@users.noreply.github.com>
2023-03-29 17:05:44 +0900

    Update Player.cs

--- SMG-Project/Assets/Scripts/Player.cs
+++ SMG-Project/Assets/Scripts/Player.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera camera;
+    private Animator animator;
+
+    private bool isMove;
+    private Vector3 destination;
+    private float speed;
+
+    private void Awake()
     {
-        
+        camera = Camera.main;
+        animator = GetComponentInChildren<Animator>();
+        speed = 5.0f;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+    }
+
     void Update()
     {
-        
+        CheckDestination();
+        Move();
     }
+
+    private void CheckDestination()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                SetDestination(hit.point);
+            }
+        }
+    }
+    private void SetDestination(Vector3 dest)
+    {
+        destination = dest;
+        isMove = true;
+        animator.SetBool("IsMove", true);
+    }
+
+    private void Move()
+    {
+        if (isMove)
+        {
+            var dir = destination - transform.position;
+            animator.transform.forward = dir;
+            transform.position += dir.normalized * Time.deltaTime * speed;
+        }
+
+        if (Vector3.Distance(transform.position, destination) <= 0.1f)
+        {
+            isMove = false;
+            animator.SetBool("IsMove", false);
+        }
+    }
+
 }
